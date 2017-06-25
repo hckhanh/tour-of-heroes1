318d3e970aa6e251800dbb612e59e1be35bdd67b
Khanh Hoang <hoangchaukhanh93@gmail.com>
2017-06-25 22:17:58 +0700

    Add App and HeroDetail components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Hero } from './hero'
+import { HeroService } from './hero.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  heroes: Hero[]
+  selectedHero: Hero
+
+  constructor(private heroService: HeroService) {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero
+  }
+
+  ngOnInit(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes)
+  }
 }
