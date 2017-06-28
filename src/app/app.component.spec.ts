bc454eb41485a187876df63018dafd6573433a5a
Khanh Hoang <hoangchaukhanh93@gmail.com>
2017-06-29 02:14:31 +0700

    Add tests for AppComponent and HeroDetailComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
-import { TestBed, async } from '@angular/core/testing';
-
-import { AppComponent } from './app.component';
+import { async, TestBed } from '@angular/core/testing'
+import { RouterTestingModule } from '@angular/router/testing'
+import { AppComponent } from './app.component'
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -8,25 +8,35 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
-  }));
+      imports: [
+        RouterTestingModule
+      ]
+    }).compileComponents()
+  }))
 
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+    const fixture = TestBed.createComponent(AppComponent)
+    const app = fixture.debugElement.componentInstance
+    expect(app).toBeTruthy()
+  }))
 
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
+  // it(`should have title 'Tour of Heroes'`, async(() => {
+  //   const fixture = TestBed.createComponent(AppComponent)
+  //   const app = fixture.debugElement.componentInstance
+  //   expect(app.title).toEqual('app')
+  // }))
 
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
-  }));
-});
+  it(`should have title 'Tour of Heroes'`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent)
+    fixture.detectChanges()
+    const compiled = fixture.debugElement.nativeElement
+    expect(compiled.querySelector('h1').textContent).toContain('Tour of Heroes')
+  }))
+
+  it('should render 2 links', async(() => {
+    const fixture = TestBed.createComponent(AppComponent)
+    fixture.detectChanges()
+    const compiled = fixture.debugElement.nativeElement
+    expect(compiled.querySelector('nav').childElementCount).toBe(2)
+  }))
+})
--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,6 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
-
+import { FormsModule } from '@angular/forms'
+import { RouterTestingModule } from '@angular/router/testing'
+import { HeroService } from '../hero.service'
+import { HEROES_DATA } from '../mockup-data'
 import { HeroDetailComponent } from './hero-detail.component'
+import createSpy = jasmine.createSpy
+
+class HeroServiceStub {
+  getHero = createSpy('getHero').and.callFake(() =>
+    Promise
+      .resolve(true)
+      .then(() => Object.assign({}, HEROES_DATA[0]))
+  )
+}
 
 describe('HeroDetailComponent', () => {
   let component: HeroDetailComponent
@@ -8,7 +20,14 @@ describe('HeroDetailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-        declarations: [HeroDetailComponent]
+        declarations: [HeroDetailComponent],
+        imports: [
+          RouterTestingModule,
+          FormsModule
+        ],
+        providers: [
+          { provide: HeroService, useClass: HeroServiceStub }
+        ]
       })
       .compileComponents()
   }))
@@ -16,6 +35,9 @@ describe('HeroDetailComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HeroDetailComponent)
     component = fixture.componentInstance
+    // component.hero = HEROES_DATA[0]
+    // const expectedHero = new Hero(42, 'Test Name')
+    // component.hero = expectedHero
     fixture.detectChanges()
   })
 
