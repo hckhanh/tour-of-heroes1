76205e5b4643f5d5a81575393f2d638d7029615a
Khanh Hoang <hoangchaukhanh93@gmail.com>
2017-06-30 09:33:40 +0700

    Update tests for HeroService

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -1,4 +1,4 @@
-import { async, inject, TestBed } from '@angular/core/testing'
+import { async, TestBed } from '@angular/core/testing'
 import { HttpModule } from '@angular/http'
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api'
 import { Hero } from './hero'
@@ -7,36 +7,40 @@ import { InMemoryDataService } from './in-memory-data.service'
 import { HEROES_DATA } from './mockup-data'
 
 describe('HeroService', () => {
+  let heroService: HeroService
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
+    const injector = TestBed.configureTestingModule({
       imports: [
         HttpModule,
         InMemoryWebApiModule.forRoot(InMemoryDataService)
       ],
       providers: [HeroService]
     })
+
+    heroService = injector.get(HeroService)
   })
 
-  it('should be created', inject([HeroService], (service: HeroService) => {
-    expect(service).toBeTruthy()
+  it('should be created', (() => {
+    expect(heroService).toBeTruthy()
   }))
 
-  it('#getHeroes should get a list of Heroes', async(inject([HeroService], (service: HeroService) => {
-    service
+  it('#getHeroes should get a list of Heroes', async(() => {
+    heroService
       .getHeroes()
       .then(heroes => expect(heroes).toEqual(HEROES_DATA))
-  })))
+  }))
 
-  it('#getHeroes get a hero (id: 20) in database', async(inject([HeroService], (service: HeroService) => {
+  it('#getHeroes get a hero (id: 20) in database', async(() => {
     const expectedHero = HEROES_DATA.find((hero: Hero) => hero.id === 20)
-    service
+    heroService
       .getHero(20)
       .then(hero => expect(hero).toEqual(expectedHero))
-  })))
+  }))
 
-  it('#getHeroes get a hero (id: -1) does not existed in database', async(inject([HeroService], (service: HeroService) => {
-    service
+  it('#getHeroes get a hero (id: -1) does not existed in database', async(() => {
+    heroService
       .getHero(-1)
       .catch(error => expect(error).toBeTruthy())
-  })))
+  }))
 })
